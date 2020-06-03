ce9b912a4e2b8545e249457853569248c589187b
Kristina Markova <kristinamarkovaa@gmail.com>
2020-06-03 23:35:13 +0200

    Changing driver's path

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -10,8 +10,8 @@ import java.util.concurrent.TimeUnit;
 public class BaseTest {
 
 
-    //String driverPath = "C:\\Drivers\\Chrome drivers\\Chrome driver 81.0.4044.69\\chromedriver.exe"; //private
-    String driverPath = "C:\\Users\\Kristina.Markova\\IdeaProjects\\neptun-automation-tests\\src\\test\\resources\\drivers\\chromedriver.exe"; //work
+    String driverPath = "C:\\Drivers\\Chrome drivers\\Chrome 83.0.4103.14\\chromedriver.exe"; //private
+    //String driverPath = "C:\\Users\\Kristina.Markova\\IdeaProjects\\neptun-automation-tests\\src\\test\\resources\\drivers\\chromedriver.exe"; //work
     String url = "https://setec.mk/index.php";
     public static WebDriver driver;
 
